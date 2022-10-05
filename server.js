38f207fe4e16801563505f11d67909c6a3c87c58
NinaSerebryakova <serebr.nina@gmail.com>
2022-10-04 22:22:01 -0400

    added handling requests for one animal or a list of animals, deployed to heroku

--- server.js
+++ server.js
@@ -35,6 +35,10 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
+function findById(id, animalsAray) {
+    const result = animalsAray.filter(animal => animal.id === id)[0];
+    return result;
+}
 
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -43,6 +47,16 @@ app.get('/api/animals', (req, res) => {
     }
     res.json(results);
 });
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+});
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${3001}!`);
 });
